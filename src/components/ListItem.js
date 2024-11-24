c150599b1033c143b455c4483c4057f3e4d2e594
ururu42 <utepusechka@gmail.com>
2024-11-24 16:38:47 +0100

    Финальный проект (без доп. задания)

---
+++ src/components/ListItem.js
@@ -0,0 +1,36 @@
+import { Component } from "../core/Component";
+import dayjs from "dayjs"; // ES 2015
+import { App } from "./App";
+
+export class ListItem extends Component {
+  setup(props) {
+    this.state = {
+      id: Date.now(),
+      date: dayjs(new Date()).format("DD/MM/YYYY, HH:mm:ss - "),
+      amount: props.amount,
+    };
+
+    this.$rootElement = document.createElement("div");
+    this.$rootElement.className = "donate-item";
+    this.$rootElement.textContent = `${this.state.date}`;
+
+    this.b = document.createElement("b");
+    this.b.textContent = ` $${this.props.amount}`;
+
+    this.$rootElement.appendChild(this.b);
+
+    this.button = document.createElement("button");
+    this.button.className = "delete-button";
+    this.button.textContent = "Удалить";
+
+    this.$rootElement.appendChild(this.button);
+
+    console.log("this.state.amount", this.state.amount); // здесь число доната
+
+    // this.button.addEventListener("click", (event) => {
+    //   if (event.target) {
+    //     this.props.onDelete(this.state.id);
+    //   }
+    // });
+  }
+}
